81293023aaee0a2c79f99f772fd4c7c66ed3942b
zheng0zhou <809875104@qq.com>
2020-12-05 18:03:37 +0100

    add Autoamtion Resource

--- WebService/AutomationService/src/main/java/fr/insa/mas/AutomationService/controller/AutomationServiceResource.java
+++ WebService/AutomationService/src/main/java/fr/insa/mas/AutomationService/controller/AutomationServiceResource.java
@@ -1,5 +1,53 @@
 package fr.insa.mas.AutomationService.controller;
 
-public class AutomationServiceResource {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import fr.insa.mas.AutomationService.model.Shutter;
+
+@RestController
+@RequestMapping("/auto")
+public class AutomationServiceResource {
+	
+	private final String luminosityURI = "http://licalhost:4200/luminosity/";
+	private final String shutterURI = "http://localhost:4201/shutter/";
+	
+	@GetMapping("/run")
+	public String run(){
+		RestTemplate restTemplate = new RestTemplate();
+		
+		String msg="";
+		
+		//Récupération de la valeur d'ouverture courante, en%
+		double currentOpeningPercent = restTemplate.getForObject(shutterURI+"opening?unit=percent",Double.class);
+		msg += "Ouverture actuelle : "+ currentOpeningPercent+" %";
+		
+		//Récupération de l'objet Shutter
+		Shutter shutter = restTemplate.getForObject(shutterURI, Shutter.class);
+		msg += "### Commande actuelle  : "+shutter.getOrder();
+		
+		//Récupération de la valeur de luminosité
+		int luminosity = restTemplate.getForObject(luminosityURI+"value", Integer.class);
+		msg += "--- La luminosité est de "+ luminosity+"\n";
+		
+		if(luminosity>50){
+			msg+=" | Forte luminosité -> il faut fermer les volets";
+			double order = (100 - luminosity)/100.0;
+			shutter.setOrder(order);
+		}else{
+			msg += " | Faible luminosité -> il faut ouvrir les volets";
+			double order = 0.005 + (100 - luminosity)/100.0;
+			shutter.setOrder(order);
+		}
+		//Requête de mise à jour du Shutter
+		restTemplate.put(shutterURI, shutter);
+		
+		//Récupération du nouveau statut de l'objet Shutter
+		String newStatus = restTemplate.getForObject(shutterURI+"status", String.class);
+		msg += " *** Le statut du volet est : " + newStatus;
+		
+		return msg;
+	}
 }
---
+++ WebService/AutomationService/src/main/java/fr/insa/mas/AutomationService/model/Shutter.java
@@ -0,0 +1,44 @@
+package fr.insa.mas.AutomationService.model;
+
+public class Shutter {
+	
+	
+	private double currentState;
+	private double order;
+	private boolean isMoving;
+	
+	public Shutter() {}
+
+	public Shutter(double currentState, double order, boolean isMoving) {
+		super();
+		this.currentState = currentState;
+		this.order = order;
+		updateIsMoving();
+	}
+	
+	public double getCurrentState() {
+		return currentState;
+	}
+
+	public void setCurrentState(double currentState) {
+		this.currentState = currentState;
+	}
+
+	public double getOrder() {
+		return order;
+	}
+
+	public void setOrder(double order) {
+		this.order = order;
+	}
+
+	public boolean isMoving() {
+		return isMoving;
+	}
+	
+	public void updateIsMoving() {
+		this.isMoving = !(this.order == this.currentState);
+	}
+	
+	
+}
