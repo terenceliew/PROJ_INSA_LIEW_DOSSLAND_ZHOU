6db51e16c8721ccfee2a66b4fd689521a3522965
Hermund Dossland <hermundld@gmail.com>
2020-12-06 19:21:08 +0100

    Added luminosity sensor service

---
+++ WebService/LuminosityService/src/main/java/fr/insa/mas/LuminosityService/LuminosityServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.mas.LuminosityService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminosityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuminosityServiceApplication.class, args);
+	}
+
+}
