368488cb7b1f2dba601e85061e7df4db520e9f49
terenceliew <liew@etud.insa-toulouse.fr>
2020-12-08 21:25:37 +0100

    added dependencies in pom to avoid using .jar, succeed in parsing xml to java object and displaying the java object with json for IndoorTemperatureService

--- WebService/IndoorTemperatureService/src/main/java/fr/insa/mas/IndoorTemperatureService/controller/IndoorTemperatureServiceResource.java
+++ WebService/IndoorTemperatureService/src/main/java/fr/insa/mas/IndoorTemperatureService/controller/IndoorTemperatureServiceResource.java
@@ -2,6 +2,7 @@ package fr.insa.mas.IndoorTemperatureService.controller;
 
 import java.util.Collections;
 
+import org.eclipse.om2m.commons.resource.ContentInstance;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import fr.insa.mas.IndoorTemperatureService.model.ContentInstance;
+
 import fr.insa.mas.IndoorTemperatureService.model.IndoorTemperature;
 import fr.insa.mas.IndoorTemperatureService.model.Mapper;
 
@@ -21,8 +22,8 @@ import fr.insa.mas.IndoorTemperatureService.model.Mapper;
 @RequestMapping("/indoorTemp")
 public class IndoorTemperatureServiceResource {
 	
-	@GetMapping(value="/value/{room}", produces = MediaType.APPLICATION_XML_VALUE)
-	public String getIndoorTemp(@PathVariable int room){ //TODO : Change return to IndoorTemperature 
+	@GetMapping(value="/value/{room}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public IndoorTemperature getIndoorTemp(@PathVariable int room){ //TODO : Change return to IndoorTemperature 
 		
 		
 		// request url
@@ -39,7 +40,6 @@ public class IndoorTemperatureServiceResource {
 		headers.set("Accept", "application/xml");
 		
 //		headers.setContentType(MediaType.APPLICATION_JSON);
-//		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_XML));
 		
 		// build the request
 		HttpEntity<String> request = new HttpEntity<String>("parameters",headers);
@@ -52,19 +52,23 @@ public class IndoorTemperatureServiceResource {
 		        String.class
 		);
 		
-		String resp = response.getBody();
+		String resp = (String)response.getBody();
 		
 		System.out.println(resp);
-		System.out.println ("************B");
+
 		Mapper mapper = new Mapper();
-		System.out.println ("************A");
+
 		ContentInstance cin = (ContentInstance) mapper.unmarshal(resp);
 		
-		
-//		IndoorTemperature indoorTemp = new IndoorTemperature(room, Double.parseDouble(cin.getContent()));
 		System.out.println ("************");
-		System.out.println (cin.getContent());
-		return resp;
+		
+		double indoorTempVal = Double.parseDouble(cin.getContent());
+		
+		System.out.println (indoorTempVal);
+		
+		IndoorTemperature indoorTemp = new IndoorTemperature(room, indoorTempVal);
+
+		return indoorTemp;
 //		int outdoorTemp = temp.getOutdoorTemp();
 		
 //		Temperature temp = restTemplate.getForObject("http://localhost:8080/~/mn-cse/mn-name/OutdoorTemp_1/DATA/la", Temperature.class);
